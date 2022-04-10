06d904f4a0fd04986a98fb8e4c8ee8f97fce3120
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-04-10 19:13:07 -0400

    mui init main page

--- taskmanager/ClientApp/empmanager/src/App.js
+++ taskmanager/ClientApp/empmanager/src/App.js
@@ -1,22 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MainPage from "./components/nav/MainPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Alhamdulillah
-        </a>
+        <MainPage></MainPage>
       </header>
     </div>
   );
